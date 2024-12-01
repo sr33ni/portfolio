0859eb5895570bfe1da814b19b87e7c6a9de88eb
sreeni <sreenivas.s@zerone-consulting.com>
2024-12-01 19:23:04 +0530

    new change portfolio two

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
+const menuNav = document.querySelectorAll('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
 let showMenu = false;
@@ -12,16 +12,30 @@ function toggleMenu() {
   if(!showMenu) {
     hamburger.classList.add('open');
     nav.classList.add('open');
-    menuNav.classList.add('open');
+    menuNav.forEach(item => item.classList.add('open'))
     navItems.forEach(item => item.classList.add('open'));
 
     showMenu = true;
   } else {
     hamburger.classList.remove('open');
     nav.classList.remove('open');
-    menuNav.classList.remove('open');
+    menuNav.forEach(item => item.classList.remove('open'))
     navItems.forEach(item => item.classList.remove('open'));
 
     showMenu = false;
   }
 }
+
+let setName = document.getElementById("name");
+let setInfo = document.getElementById("info") 
+let textinit = 0 ;
+let textinfo = "A UI Developer crafting intuitive, engaging interfaces"
+let textstart = 'Nice meeting you';
+let textspeed = 50;
+window.onload = function typing() {
+if(  textinit < textinfo.length){
+    // setName.innerHTML += textstart.charAt(textinit);
+    setInfo.innerHTML += textinfo.charAt(textinit)
+    textinit++
+    setTimeout(typing, textspeed)
+}}
