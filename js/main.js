c5f54834e1318837f292c50efecf77b4347786dc
sreeni <sreenivas.s@zerone-consulting.com>
2025-11-18 10:51:41 +0530

    new changes | c04

--- js/main.js
+++ js/main.js
@@ -39,3 +39,21 @@ if(  textinit < textinfo.length){
     textinit++
     setTimeout(typing, textspeed)
 }}
+
+const boxes = document.querySelectorAll('.box-to-observe-top, .box-to-observe-bottom');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active'); 
+    } else {
+      entry.target.classList.remove('active');
+    }
+  });
+}, {
+  root: null,
+  rootMargin: "0px 0px 20px 0px",
+  threshold: 0
+});
+
+boxes.forEach(box => observer.observe(box));
